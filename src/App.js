41ffbffc45568637634f82fa30220867f0fa5ee3
Batbakarrik <batbakarrik@outlook.com>
2019-09-20 22:10:50 +0200

    Standard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import Form from './components/Form';
+import Form from './components/Form'
 
 const Description = styled.p`
   color: black;
@@ -12,30 +12,30 @@ const Description = styled.p`
   &:hover {
     color: purple;
   }
-`;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <Description> <li>Liste</li>
         </Description>
-        <Form></Form>
+        <Form />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
